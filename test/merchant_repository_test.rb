f77420c78eca412c1c911c5fbb39c3882539629f
Samson Brock <SamsonLB@gmail.com>
2015-06-24 12:50:40 -0600

    Add Merchant.customers_with_pending_invoices

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -109,5 +109,60 @@ class MerchantRepositoryTest < Minitest::Test
     # 5) 1102.12
     assert_equal [3, 4, 2], merchant_repo.most_revenue(3).map { |merchant| merchant.id }
   end
+
+  def test_most_items
+    sales_engine = SalesEngine.new
+    merchant_repo = MerchantRepository.new(
+        [{ id: 1 },
+         { id: 2 },
+         { id: 3 }],
+        sales_engine)
+    sales_engine.item_repository = ItemRepository.new(
+        [{ id: 10, merchant_id: 1 },
+         { id: 20, merchant_id: 2 },
+         { id: 30, merchant_id: 2 },
+         { id: 40, merchant_id: 3 },
+         { id: 50, merchant_id: 3 },
+         { id: 60, merchant_id: 3 },
+         { id: 70, merchant_id: 1 },
+         { id: 80, merchant_id: 1 }],
+        sales_engine)
+    sales_engine.invoice_repository = InvoiceRepository.new(
+        [{ id:  10000, merchant_id: 1 },
+         { id:  20000, merchant_id: 2 },
+         { id:  30000, merchant_id: 2 },
+         { id:  40000, merchant_id: 3 },
+         { id:  50000, merchant_id: 3 }],
+        sales_engine)
+    sales_engine.transaction_repository = TransactionRepository.new(
+        [{ id:  1000, invoice_id:  10000, result: "failed" },
+         { id:  2000, invoice_id:  10000, result: "success" },
+         { id:  3000, invoice_id:  20000, result: "failed" },
+         { id:  4000, invoice_id:  20000, result: "failed" },
+         { id:  5000, invoice_id:  30000, result: "success" },
+         { id:  6000, invoice_id:  40000, result: "failed" },
+         { id:  7000, invoice_id:  40000, result: "failed" },
+         { id:  8000, invoice_id:  40000, result: "success" },
+         { id:  9000, invoice_id:  50000, result: "failed" },
+         { id: 10000, invoice_id:  50000, result: "success" }],
+        sales_engine)
+    sales_engine.invoice_item_repository = InvoiceItemRepository.new(
+        [{ id:  100, invoice_id: 10000, item_id: 10, quantity: 2 },
+         { id:  200, invoice_id: 20000, item_id: 20, quantity: 4 },
+         { id:  300, invoice_id: 30000, item_id: 30, quantity: 2 },
+         { id:  400, invoice_id: 40000, item_id: 40, quantity: 3 },
+         { id:  500, invoice_id: 50000, item_id: 50, quantity: 1 },
+         { id:  600, invoice_id: 50000, item_id: 60, quantity: 3 },
+         { id:  700, invoice_id: 10000, item_id: 70, quantity: 2 },
+         { id:  700, invoice_id: 10000, item_id: 80, quantity: 2 }],
+        sales_engine)
+
+    # TODO: Add InvoiceRepository and TransactionRepository
+    # 1: 2 + 2 + 2 = 6
+    # 2: 2
+    # 3: 3 + 1 + 3 = 7
+    # 1: 2 + 4 + 1 + 1 = 8, 2: 2 + 2 = 4, 3: 1 + 3 + 2 = 6
+    assert_equal [3, 1], merchant_repo.most_items(2).map { |merchant| merchant.id }
+  end
 end
 
